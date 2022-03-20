f4259d573e8a06553c7791abaed94ed393328b67
Anđela <92460012+AndelaD09@users.noreply.github.com>
2022-03-20 21:32:54 +0100

    Delete MySmartRegister/dto directory

--- MySmartRegister/dto/CathegoryDTO.java
+++
@@ -1,9 +0,0 @@
-package com.application.dto;
-
-import lombok.Data;
-
-@Data
-public class CathegoryDTO {
-    private Long id;
-    private String name;
-}
--- MySmartRegister/dto/RoleDTO.java
+++
@@ -1,9 +0,0 @@
-package com.application.dto;
-
-import lombok.Data;
-
-@Data
-public class RoleDTO {
-    private Long ID;
-    private String name;
-}
