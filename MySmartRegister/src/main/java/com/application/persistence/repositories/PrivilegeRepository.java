73b8bce00e07f1b3c68365f926b6d66358fd28ec
Anđela <92460012+AndelaD09@users.noreply.github.com>
2022-03-30 16:31:02 +0200

    Delete MySmartRegister/src/main/java/com/application/persistence directory

--- MySmartRegister/src/main/java/com/application/persistence/repositories/PrivilegeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.application.persistence.repositories;
-
-import com.application.persistence.entities.Privilege;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
-    Privilege findByName(String name);
-}
